8a2816e7a340858e319043d6fd08bc041608755b
Aleksandar Stojanoski <aleksandar-stojanoski@hotmail.com>
2019-07-27 14:58:44 +0200

    update

--- debloater.ps1
+++ debloater.ps1
@@ -1,25 +1,31 @@
 # List all preinstalled applications
-Get-AppxPackage | ft Name, PackageFullName -AutoSize
+Get-AppxPackage | ft Name, PackageFullName -AutoSize;
 
 # Applications to be removed
-Get-AppxPackage *xboxapp* | Remove-AppxPackage
-Get-AppxPackage *BingWeather* | Remove-AppxPackage
-Get-AppxPackage *GetStarted* | Remove-AppxPackage
-Get-AppxPackage *MicrosoftOfficeHub* | Remove-AppxPackage
-Get-AppxPackage *MicrosoftSolitaireCollection* | Remove-AppxPackage
-Get-AppxPackage *MicrosoftStickyNotes* | Remove-AppxPackage
-Get-AppxPackage *OneNote* | Remove-AppxPackage
-Get-AppxPackage *OneConnect* | Remove-AppxPackage
-Get-AppxPackage *People* | Remove-AppxPackage
-Get-AppxPackage *Alarms* | Remove-AppxPackage
-Get-AppxPackage *Maps* | Remove-AppxPackage
-Get-AppxPackage *Phone* | Remove-AppxPackage
-Get-AppxPackage *Minecraft* | Remove-AppxPackage
-Get-AppxPackage *Messaging* | Remove-AppxPackage
-Get-AppxPackage *WindowsFeedbackHub* | Remove-AppxPackage
-Get-AppxPackage *XboxSpeechToTextOverlay* | Remove-AppxPackage
-Get-AppxPackage *Xbox* | Remove-AppxPackage
-Get-AppxPackage *Print3D* | Remove-AppxPackage
+$appList =
+    "xboxapp",
+    "BingWeather",
+    "GetStarted",
+    "MicrosoftOfficeHub",
+    "MicrosoftSolitaireCollection",
+    "MicrosoftStickyNotes",
+    "OneNote",
+    "OneConnect",
+    "People",
+    "Alarms",
+    "Maps",
+    "Phone",
+    "Minecraft",
+    "Messaging",
+    "WindowsFeedbackHub",
+    "XboxSpeechToTextOverlay",
+    "Xbox",
+    "Print3D";
+
+# Remove applications
+foreach ($app in $appList) {
+    Get-AppxPackage *$app* | Remove-AppxPackage;
+}
 
 # List remaining applications
-Get-AppxPackage | ft Name, PackageFullName -AutoSize
+Get-AppxPackage | ft Name, PackageFullName -AutoSize;
